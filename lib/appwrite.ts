bfe48735d4b2793713ecf208d2753ae79f71fe96
Midhun Joseph <midhuntjoseph03@gmail.com>
2026-04-09 16:52:16 +0530

    google auth is completed and globel conetxt is added so the auth credetial can be used in entire application

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -80,7 +80,6 @@ export async function getCurrentUser() {
             }
             
             } catch(e){
-                console.error(e);
-                return false;
+                return null;
             }
 }
