5e0471c6901aee97636b27dea8958456071717d0
Midhun Joseph <midhuntjoseph03@gmail.com>
2026-04-25 16:47:47 +0530

    core profile and home is implemented

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,17 +1,39 @@
+import Search from "@/components/search";
+import icons from "@/constants/icons";
+import images from "@/constants/images";
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+
+import { Text, View ,Image, TouchableOpacity} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <SafeAreaView className="bg-white h-full">
+      <View className="px-5">
+        <View className="flex felx-row itmes-center justify-between mt-5">
+          <View className="flex flex-row items-center">
+           <Image source={images.avatar} className="size-12 rounded-full"/>
+           <View className="flex flex-col items-start ml-2 justify-center">
+            <Text className="text-xs font-rubik text-black-100">Good Morning</Text>
+            <Text className="font-rubik-medium text-black-300">Midhun</Text>
+           </View>
+           <Image source={icons.bell} className="size-6  ml-10"/>
+          </View>
+        </View>
+        <Search/>
 
-      <Text className="font-bold text-lg my-10"> react app</Text>
+      <View>
+        <View className="flex flex-row items-center justify-between">
+          <Text className="text-xl font-rubik-bold text-black-300">Featured</Text>
+          <TouchableOpacity>
+            <Text className="text-base font-rubik-bold text-primary-300">See All</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+      
+      </View>
 
-      <Link href="/sign-in">Sign in</Link>
-      <Link href="/explore">Explore</Link>
-      <Link href="/profile">Profile</Link>
-      <Link href="/properties/1">Property</Link>
-
-    </View>
+      
+    </SafeAreaView>
   );
 }
--- app/(root)/(tabs)/profile.tsx
+++ app/(root)/(tabs)/profile.tsx
@@ -1,11 +1,94 @@
+import { settings } from '@/constants/data';
+import icons from '@/constants/icons';
+import images from '@/constants/images';
+import { logout } from '@/lib/appwrite';
+import { useGlobalContext } from '@/lib/global-provider';
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { Alert, Image, ImageSourcePropType, ScrollView, Text, TouchableOpacity, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+interface SettingsItemProps {
+    icon : ImageSourcePropType;
+    title : string;
+    onpress ?: () => void;
+    textStyle ?:any;
+    showArrow ?:boolean
+}
+
+const SettingsItems = ({icon ,title , onpress,textStyle,showArrow=true}
+    : SettingsItemProps) => (
+    <TouchableOpacity onPress={onpress} className='flex flex-row items-center justify-between py-3'>
+        <View>
+            <Image source={icon} className='size-6'/>
+            <Text className={`text-lg font-rubik-medium text-black-300 ${textStyle}`}>{title} </Text>
+        </View>
+
+        {showArrow && <Image source={icons.rightArrow} className='size-6'/>}
+    </TouchableOpacity>
+)
 
 const Profile = () => {
+
+    const {user ,refetch} = useGlobalContext();
+
+
+    const handleLogout = async () => {
+        const result = await logout();
+
+        if(result){
+            Alert.alert('success',"you have succesfully loged out!");
+
+            refetch();
+        }else{
+            Alert.alert("error","error while logging in !");
+        }
+    };
+
     return (
-        <View>
-            
-        </View>
+       <SafeAreaView className='h-full bg-white'>
+        <ScrollView
+         showsVerticalScrollIndicator={false}
+         contentContainerClassName='pb-32 px-7'>
+
+            <View className='flex flex-row items-center justify-between mt-2'>
+                <Text className='text-xl font-rubik-bold'> Profile</Text>
+                <Image source={icons.bell} className='size-6'/> 
+            </View>
+
+            <View className='flex-row justify-center flex mt-5'>
+                <View className='flex flex-col items-center relative mt-5'>
+                    <Image source={{uri:user?.avatar}} 
+                    className='size-36 relative rounded-full'/>
+                    
+                    <TouchableOpacity className='absolute size-16 bottom-2 right-20'>
+                        <Image source={icons.edit} className='size-10'></Image>
+                    </TouchableOpacity>
+
+                    <Text className='text-2xl font-rubik-bold mt-1'>{user?.name}</Text>
+                    
+                </View>
+            </View>
+
+            <View className='flex flex-col mt-10'>
+                <SettingsItems icon={icons.calendar} title='My Bookings' />
+                 <SettingsItems icon={icons.wallet} title='Payments' />
+            </View>
+
+            <View className='flex flex-col mt-5 border-t pt-5 border-primary-200'> 
+                {settings.slice(2).map((item,index) => (
+                    <SettingsItems key={index} {...item}/>
+                ))}
+
+            </View>
+            <View className='flex flex-col mt-10'>
+                <SettingsItems icon={icons.logout} title='Logout' textStyle="text-danger" showArrow={false} onpress={handleLogout} />
+                 
+            </View>
+
+
+        </ScrollView>
+        
+       </SafeAreaView>
     );
 }
 
